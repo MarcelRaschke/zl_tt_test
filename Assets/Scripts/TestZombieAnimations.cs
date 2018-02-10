012586ec5c9c321fde07172a3dc18f0c94d3cd80
cillroy <curt@cullens.com>
2018-02-09 16:04:31 -0800

    first push

---
+++ Assets/Scripts/TestZombieAnimations.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestZombieAnimations : MonoBehaviour
+{
+    public Animation myAnim;
+    public GameObject zombie;
+
+    // Use this for initialization
+    void Start()
+    {
+		myAnim = zombie.GetComponent<Animation>();
+        foreach (AnimationState state in myAnim) {
+            state.speed = 0.5F;
+			Debug.Log(state.name);
+        }
+    }
+
+    /// Awake is called when the script instance is being loaded.
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
