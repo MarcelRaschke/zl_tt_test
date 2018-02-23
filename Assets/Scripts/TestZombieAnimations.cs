0e48be8fddd36624fa56cc1c5a84844d728c9c5a
cillroy <curt@cullens.com>
2018-02-23 00:40:31 -0800

    Updated Random Tile Coloring

--- Assets/Scripts/TestZombieAnimations.cs
+++ Assets/Scripts/TestZombieAnimations.cs
@@ -5,28 +5,32 @@ using UnityEngine.UI;
 
 public class TestZombieAnimations : MonoBehaviour
 {
-    public GameObject zombie_Animation;
-    public List<AnimationState> zStates = new List<AnimationState>();
-    public Animation myAnim;
-    public GameObject zombie;
+    public List<AnimationState> zombieStates = new List<AnimationState>();
+    public GameObject[] tileCollection;
+    public Material[] tileMaterialCollection;
+    public List<string> zombieOptions = new List<string>();
+    public Animation zombieAnimationStates;
+    public Dropdown zombieActionPicker;
+    public GameObject zombieAnimation;
     public Text debugText;
-    public Dropdown zAction;
-    public List<string> zOptions = new List<string>();
+    public bool showDebug = false;
 
     // Use this for initialization
     void Start()
     {
-        zAction.onValueChanged.AddListener(delegate { zActionChanged(zAction); });
-        zAction.ClearOptions();
-        myAnim = zombie_Animation.GetComponent<Animation>();
-        foreach (AnimationState state in myAnim)
+        getTileCollection();
+        debugText.enabled = false;
+        zombieActionPicker.onValueChanged.AddListener(delegate { zombieActionChanged(zombieActionPicker); });
+        zombieActionPicker.ClearOptions();
+        zombieAnimationStates = zombieAnimation.GetComponent<Animation>();
+        foreach (AnimationState state in zombieAnimationStates)
         {
-            zStates.Add(state);
-            zOptions.Add(state.name);
+            zombieStates.Add(state);
+            zombieOptions.Add(state.name);
             updateDebug(state.name);
         }
-        updateDebug("AnimationStates: " + zStates.Count.ToString());
-        zAction.AddOptions(zOptions);
+        updateDebug("AnimationStates: " + zombieStates.Count.ToString());
+        zombieActionPicker.AddOptions(zombieOptions);
     }
 
     /// Awake is called when the script instance is being loaded.
@@ -38,23 +42,42 @@ public class TestZombieAnimations : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-    }
-
-    private void zActionChanged(UnityEngine.UI.Dropdown target)
-    {
-        updateDebug(target.value.ToString());
-        setZombieAction(target.value);
+        debugText.enabled = showDebug;
     }
 
     void setZombieAction(int action)
     {
-        updateDebug(zStates[action].name);
-        myAnim.Play(zStates[action].name);
+        updateDebug(zombieStates[action].name);
+        zombieAnimationStates.Play(zombieStates[action].name);
+    }
+
+    void getTileCollection()
+    {
+        tileCollection = GameObject.FindGameObjectsWithTag("GameTile");
+        updateDebug("tileCollection: " + tileCollection.Length.ToString());
+
+        int i = 0;
+        foreach (GameObject tile in tileCollection)
+        {
+            i = Random.Range(0,tileMaterialCollection.Length);
+            updateDebug("Random number: " + i);
+            updateDebug("Tile: " + tile.name);
+            updateDebug("Tile Color: " + tileMaterialCollection[i].name);
+            tile.GetComponent<Renderer>().material = tileMaterialCollection[i];
+        }
     }
     void updateDebug(string message)
     {
-        Debug.Log(message);
-        //test
+        if (showDebug)
+        {
+            Debug.Log(message);
+            debugText.text = message;
+        }
+    }
+
+    private void zombieActionChanged(UnityEngine.UI.Dropdown target)
+    {
+        updateDebug(target.value.ToString());
+        setZombieAction(target.value);
     }
 }
