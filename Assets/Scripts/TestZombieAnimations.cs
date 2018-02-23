f65ac7f97a77703ff0199b91d22f505547734b81
cillroy <curt@cullens.com>
2018-02-22 22:34:00 -0800

    test with unity integration and zombie parts

--- Assets/Scripts/TestZombieAnimations.cs
+++ Assets/Scripts/TestZombieAnimations.cs
@@ -5,17 +5,28 @@ using UnityEngine.UI;
 
 public class TestZombieAnimations : MonoBehaviour
 {
+    public GameObject zombie_Animation;
+    public List<AnimationState> zStates = new List<AnimationState>();
     public Animation myAnim;
     public GameObject zombie;
+    public Text debugText;
+    public Dropdown zAction;
+    public List<string> zOptions = new List<string>();
 
     // Use this for initialization
     void Start()
     {
-		myAnim = zombie.GetComponent<Animation>();
-        foreach (AnimationState state in myAnim) {
-            state.speed = 0.5F;
-			Debug.Log(state.name);
+        zAction.onValueChanged.AddListener(delegate { zActionChanged(zAction); });
+        zAction.ClearOptions();
+        myAnim = zombie_Animation.GetComponent<Animation>();
+        foreach (AnimationState state in myAnim)
+        {
+            zStates.Add(state);
+            zOptions.Add(state.name);
+            updateDebug(state.name);
         }
+        updateDebug("AnimationStates: " + zStates.Count.ToString());
+        zAction.AddOptions(zOptions);
     }
 
     /// Awake is called when the script instance is being loaded.
@@ -28,5 +39,22 @@ public class TestZombieAnimations : MonoBehaviour
     void Update()
     {
 
+    }
+
+    private void zActionChanged(UnityEngine.UI.Dropdown target)
+    {
+        updateDebug(target.value.ToString());
+        setZombieAction(target.value);
+    }
+
+    void setZombieAction(int action)
+    {
+        updateDebug(zStates[action].name);
+        myAnim.Play(zStates[action].name);
+    }
+    void updateDebug(string message)
+    {
+        Debug.Log(message);
+        //test
     }
 }
