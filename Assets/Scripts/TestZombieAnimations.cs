235da51fa619a4b1e8dd81697a9039e659925982
cillroy <curt@cullens.com>
2018-02-24 22:28:48 -0800

    added color options to tiles

--- Assets/Scripts/TestZombieAnimations.cs
+++ Assets/Scripts/TestZombieAnimations.cs
@@ -1,5 +1,4 @@
-﻿
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,19 +8,25 @@ public class TestZombieAnimations : MonoBehaviour
     public List<AnimationState> zombieStates = new List<AnimationState>();
     public List<GameObject> tileCollection;
     public Material[] tileMaterialCollection;
+    public GameObject[] signCollection;
     public List<string> zombieOptions = new List<string>();
     public Animation zombieAnimationStates;
     public Dropdown zombieActionPicker;
+    public Button drawCard;
     public GameObject zombieAnimation;
     public GameObject zombie;
     public GameObject currentTile;
     public GameObject nextTile;
+    public Text debugText;
+    public RawImage nextColor;
+    public string currentAnimation;
+    public string zombieStartAnimation = "Zombie_Idle_01";
+    public string zombieWalkAnimation = "Zombie_Walk_01";
+    public string nextTileColor;
     public int currentTileCount = 0;
     public int nextTileCount = 1;
-    public Text debugText;
     public float walkSpeed = 15;
     public float turnSpeed = 5;
-    public string currentAnimation;
     public bool moveReverse = false;
     public bool foundWinner = false;
     public bool showDebug = false;
@@ -32,21 +37,24 @@ public class TestZombieAnimations : MonoBehaviour
         getTileCollection();
         currentTile = tileCollection[currentTileCount];
         nextTile = tileCollection[nextTileCount];
-        debugText.enabled = false;
+
+        drawCard.onClick.AddListener(delegate { pickCard(); });
+
         zombieActionPicker.onValueChanged.AddListener(delegate { zombieActionChanged(zombieActionPicker); });
         zombieActionPicker.ClearOptions();
+
         zombieAnimationStates = zombieAnimation.GetComponent<Animation>();
 
         foreach (AnimationState state in zombieAnimationStates)
         {
             zombieStates.Add(state);
             zombieOptions.Add(state.name);
-            updateDebug(state.name);
+            updateDebug("Zombie Animation Found: " + state.name);
         }
 
         updateDebug("AnimationStates: " + zombieStates.Count.ToString());
         zombieActionPicker.AddOptions(zombieOptions);
-        currentAnimation = zombieOptions[0];
+        currentAnimation = zombieStartAnimation;
     }
 
     /// Awake is called when the script instance is being loaded.
@@ -62,7 +70,7 @@ public class TestZombieAnimations : MonoBehaviour
 
         debugText.enabled = showDebug;
 
-        if (currentAnimation == "Zombie_Walk_01")
+        if (currentAnimation == zombieWalkAnimation)
         {
             moveZombie();
         }
@@ -129,15 +137,28 @@ public class TestZombieAnimations : MonoBehaviour
 
         updateDebug("tileCollection: " + tileCollection.Count.ToString());
 
+        setTileColor();
+    }
+
+    void setTileColor()
+    {
         int i = 0;
-        
+        int j = 0;
+
         foreach (GameObject tile in tileCollection)
         {
             i = Random.Range(0, tileMaterialCollection.Length);
-            updateDebug("Random number: " + i);
-            updateDebug("Tile: " + tile.name);
-            updateDebug("Tile Color: " + tileMaterialCollection[i].name);
-            tile.GetComponent<Renderer>().material = tileMaterialCollection[i];
+            j = Random.Range(0, signCollection.Length);
+
+            GameObject sign = tile.transform.Find("Sign").gameObject;
+            sign = Instantiate(signCollection[j]);
+            sign.transform.position = new Vector3(0, 1, 0);
+            sign.transform.rotation = new Quaternion(270, 0, 0, 0);
+            sign.transform.localScale = new Vector3(0.001276128f,0.02208854f,0.001276128f);
+            //sign.GetComponent<Renderer>().material = tileMaterialCollection[i];
+
+            updateDebug("Tile: " + tile.name + " | Tile Color: " + tileMaterialCollection[i].name + " | Sign: " + signCollection[j].name);
+            updateDebug("Sign: " + sign.name + " | Sign Cords: " + sign.transform.rotation.x.ToString());
         }
     }
     void updateDebug(string message)
@@ -153,9 +174,19 @@ public class TestZombieAnimations : MonoBehaviour
     {
         return i1.name.CompareTo(i2.name);
     }
+
     private void zombieActionChanged(UnityEngine.UI.Dropdown target)
     {
         updateDebug(target.value.ToString());
         setZombieAction(target.value);
+    }
+
+    private void pickCard()
+    {
+        updateDebug("Picking a card");
+        int i = Random.Range(0, tileMaterialCollection.Length);
+        nextTileColor = tileMaterialCollection[i].name;
+        updateDebug("Card Color: " + nextTileColor);
+        nextColor.color = tileMaterialCollection[i].color;
     }
 }
